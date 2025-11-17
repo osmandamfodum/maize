e4e093a989bf2ce8bb3cebc4e859c374265a56fc
Osman alhussein <68653173+osmandamfodum@users.noreply.github.com>
2025-11-17 10:47:22 +0200

    Update maize_honeyapp.py

--- Maize-and-honey-Yield-Prediction-App-main/maize_honeyapp.py
+++ Maize-and-honey-Yield-Prediction-App-main/maize_honeyapp.py
@@ -18,7 +18,7 @@ st.markdown(
 
 # Display image at the top
 st.markdown('<div class="image-container">', unsafe_allow_html=True)
-st.image("neu.jpg", width='stretch')
+st.image("/neu.jpg", width='stretch')
 st.markdown('</div>', unsafe_allow_html=True)
 
 # Switch button using radio for mode selection
@@ -197,4 +197,5 @@ with st.sidebar:
     st.write(f"**Data Range**: 2000–2023 (Maize) or 1995–2021 (Honey).")
 
     st.write("**Charts**: Historical trends, comparisons, distribution, and feature importance.")
+
 
